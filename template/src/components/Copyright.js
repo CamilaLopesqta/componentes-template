71aaccbe647d0e7e5f9e8834f9575ad629baf9e6
Camila Lopes <camilalopesqta@gmail.com>
2024-02-25 17:06:56 -0300

    Exercícios da aula de Components

---
+++ template/src/components/Copyright.js
@@ -0,0 +1,11 @@
+function Copyright() {
+  const copyright = "Todos os direitos reservados à Camila.";
+  return (
+    <div className="copyright">
+      <h3>Esté é o Copyright</h3>
+      <h4>{copyright}</h4>
+    </div>
+  );
+}
+
+export default Copyright;
---
+++ template/src/components/Footer.js
@@ -0,0 +1,17 @@
+import Copyright from "./Copyright";
+import RedesSociais from "./RedesSociais";
+
+function Footer () {
+
+    return (
+<div className="footer" >
+    <h3>Este é o Footer</h3>
+    <footer>
+    <RedesSociais/>
+    <Copyright/>
+    </footer>
+</div>
+    )
+}
+
+export default Footer;
